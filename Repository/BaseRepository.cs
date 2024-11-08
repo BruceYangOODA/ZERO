a368d74df1cdd19fe3816de392b4cc7ce5a94fac
Bruce Yang <bruce.yang@pitl.com.hk>
2024-11-08 14:29:59 +0800

    feat: master3

--- Repository/BaseRepository.cs
+++ Repository/BaseRepository.cs
@@ -1,21 +1,104 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ZERO.Repository.IRepository;
 
 namespace ZERO.Repository
 {
-    public class BaseRepository<T> where T : BaseRepository<T>
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
         protected readonly IConfiguration _configuration;        
         protected readonly string _connectStringJpmedRWD;
         protected readonly string _connectStringAIShelves;
-        protected ILogger<T> _logger;
+        protected ILogger<TEntity> _logger;
         protected readonly DbContext _context;
-        public BaseRepository(IConfiguration configuration, ILogger<T> logger, DbContext context)
+        public virtual DbSet<TEntity> Table => _context.Set<TEntity>();
+        public BaseRepository(IConfiguration configuration, ILogger<TEntity> logger, DbContext context)
         {
             _context = context;
             _configuration = configuration;            
             //_connectStringJpmedRWD = _configuration.GetConnectionString("JpmedRWDMysql");
             //_connectStringAIShelves = _configuration.GetConnectionString("AIShelvesMysql");
             _logger = logger;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return Table.AsQueryable();
+        }
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Where(predicate);
+        }
+        public List<TEntity> GetAllList()
+        {
+            return GetAll().ToList();
+        }
+        public List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Where(predicate).ToList();
+        }
+        public TEntity Single(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Single(predicate);
+        }
+        public TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().FirstOrDefault(predicate);
+        }
+        public async Task<TEntity> InsertAsync(TEntity entity)
+        {
+            var entityEntry = await Table.AddAsync(entity);
+            await SaveAsync();
+            return entityEntry.Entity;
+        }
+        public async Task<List<TEntity>> InsertAsync(List<TEntity> entities)
+        {
+            await Table.AddRangeAsync(entities);
+            await SaveAsync();
+            return entities;
+        }
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            AttachIfNot(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await SaveAsync();
+            return entity;
+        }
+        public async Task DeleteAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            AttachIfNot(entity);
+            Table.Remove(entity);
+            await SaveAsync();
+        }
+        public async Task DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            foreach (var entity in GetAll().Where(predicate).ToList())
+            {
+                await DeleteAsync(entity);
+            }
+        }
+
+        /// <summary>
+        /// 檢查實體是否處於跟蹤狀態，如果是，則返回；如果不是，則添加跟蹤狀態
+        /// </summary>
+        /// <param name="entity"></param>
+        protected virtual void AttachIfNot(TEntity entity)
+        {
+            var entry = _context.ChangeTracker.Entries().FirstOrDefault(ent => ent.Entity == entity);
+            if (entry != null)
+            {
+                return;
+            }
+            Table.Attach(entity);
+        }
+        protected async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ Repository/IRepository/IBaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq.Expressions;
+
+namespace ZERO.Repository.IRepository
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        #region 查詢
+        /// <summary>
+        /// 用於獲取傳入本方法的所有實體
+        /// </summary>
+        /// <returns>所有實體列表</returns>
+        IQueryable<TEntity> GetAll();
+        /// <summary>
+        /// 用於獲取傳入本方法的所有實體 <paramref name="predicate" />
+        /// </summary>
+        /// <param name="predicate">篩選實體的條件</param>
+        /// <returns>所有實體列表</returns>
+        IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+        /// <summary>
+        /// 用於獲取傳入本方法的所有實體
+        /// </summary>
+        /// <returns>所有實體列表</returns>
+        List<TEntity> GetAllList();
+        /// <summary>
+        /// 用於獲取傳入本方法的所有實體 <paramref name="predicate" />
+        /// </summary>
+        /// <param name="predicate">篩選實體的條件</param>
+        /// <returns>所有實體列表</returns>
+        List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate);
+        /// <summary>
+        /// 通過傳入的篩選條件來獲取實體訊息
+        /// 如果查詢不到返回值，則會引起異常
+        /// </summary>
+        /// <param name="predicate">Entity</param>
+        /// <returns></returns>
+        TEntity Single(Expression<Func<TEntity, bool>> predicate);
+        /// <summary>
+        /// 通過傳入的篩選條件查詢實體訊息，如果沒有找到，則返回Null
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+        #endregion
+        /// <summary> 異步添加一個新實體訊息 </summary>
+        /// <param name="entity">被添加的實體</param>
+        /// <returns></returns>
+        Task<TEntity> InsertAsync(TEntity entity);
+        /// <summary> 異步添加多個新實體訊息 </summary>
+        /// <param name="entities">被添加的實體</param>
+        /// <returns></returns>
+        Task<List<TEntity>> InsertAsync(List<TEntity> entities);
+        /// <summary> 異步更新現有實體 </summary>
+        /// <param name="entity">Entity</param>
+        /// <returns></returns>
+        Task<TEntity> UpdateAsync(TEntity entity);
+        /// <summary> 異步刪除一個實體 </summary>
+        /// <param name="entity">無返回值</param>
+        /// <returns></returns>
+        Task DeleteAsync(TEntity entity);
+        /// <summary>
+        /// 按傳入的條件可刪除多個實體
+        /// 注意：所有符合給定條件的實體都將被檢索和刪除
+        /// 如果條件比較多，則待刪除的實體也比較多，這可能會導致主要的性能問題
+        /// </summary>
+        /// <param name="predicate">篩選實體的條件</param>
+        Task DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
